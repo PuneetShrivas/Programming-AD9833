6589fe073c7f5579275a9ac0dcf62bb85c5733d3
PuneetShrivas <puneetshrivas32@gmail.com>
2018-03-20 12:05:43 +0530

    Added initial functions

--- Programming-AD9833/Programming-AD9833/main.cpp
+++ Programming-AD9833/Programming-AD9833/main.cpp
@@ -5,9 +5,27 @@
  * Author : Puneet Shrivas
  */ 
 
+#define F_CPU 14745600
 #include <avr/io.h>
+#include <avr/delay.h>
 
+void SPI_init(void)
+{
+	DDRB|=(1<<PINB7)|(1<<PINB5)|(1<<PINB0); //sets SCK, MOSI and PINB0 as output (Fsync at PINB0)
+	PORTB|=(1<<PINB7)|(1<<PINB0);  //SCK and Fsync High
+	SPCR|=(1<<SPE)|(1<<MSTR)|(1<<CPOL); //Enable SPI, set master, prescaler = 4, SPI Mode:2
+}
 
+void SPI_Transmit(char data)
+{
+	SPDR = data;
+	while(!(SPSR & (1<<SPIF))){;/*wait for data transfer and recieving*/}
+}
+
+void SPI_write16 (char data)
+{
+	
+}	
 int main(void)
 {
     
