b360df42dd5132454dc0c777f790b7ed361d35bd
PuneetShrivas <puneetshrivas32@gmail.com>
2018-05-08 15:08:56 +0530

    Comments added

--- Programming-AD9833/Programming-AD9833/main.cpp
+++ Programming-AD9833/Programming-AD9833/main.cpp
@@ -20,30 +20,30 @@
 #include <avr/interrupt.h>
 #include <util/atomic.h>
 
-double TEMP = ((((F_CPU)/(TIMER1_PRESCALER*1000000))*532)-1);
-double TICKS = 65536-TEMP/*+2368*/;
-
-volatile float global_frequency=0;
+double TEMP = ((((F_CPU)/(TIMER1_PRESCALER*1000000))*532)-1);			//Counter Cycles for required time
+double TICKS = 65536-TEMP;												//Value for TCNT1 to implement timing by overflow
+	
+volatile float global_frequency=0;										//Volatile global variables for use in interrupt service routine
 volatile int cont=0;
 
 void SPI_init(void)
 {
-	DDRB=(1<<PINB7)|(1<<PINB5)|(1<<PINB0);         //sets SCK, MOSI,SS and PINB0 as output (F sync at Pinb0)
-	PORTB=(1<<PINB0)|(1<<PINB4);					//F sync High, SS is set high
-	SPCR=(1<<SPE)|(1<<MSTR)|(1<<CPOL)/*|(1<<SPR1)|(1<<SPR0)*/|(1<<SPIE)|(1<<SPI2X);				//Enable SPI, set master, pre-scaler = 2, SPI Mode:2
+	DDRB=(1<<PINB7)|(1<<PINB5)|(1<<PINB0);								//sets SCK, MOSI,SS and PINB0 as output (F sync at Pinb0)
+	PORTB=(1<<PINB0)|(1<<PINB4);										//F sync High, SS is set high
+	SPCR=(1<<SPE)|(1<<MSTR)|(1<<CPOL)|(1<<SPIE)|(1<<SPI2X);				//Enable SPI, set master, pre-scaler = 2, SPI Mode:2
 }
 
 void UART_init(void)
-{
-	UBRRH = (unsigned char)(BAUD_PRESCALLER>>8);
+{	
+	UBRRH = (unsigned char)(BAUD_PRESCALLER>>8);						//UART initialization from data-sheet
 	UBRRL = (unsigned char)BAUD_PRESCALLER;
 	UCSRB = (1<<RXEN)|(1<<TXEN);
-	UCSRC = (1<<URSEL)|(1<<USBS)|(3<<UCSZ0); // Set frame format: 8data, 2stop bit
+	UCSRC = (1<<URSEL)|(1<<USBS)|(3<<UCSZ0);							// Set frame format: 8data, 2stop bit
 }
 
 void UART_send(unsigned char data)
  {
-	 while(!(UCSRA & (1<<UDRE)));
+	 while(!(UCSRA & (1<<UDRE)));										//wait for buffer to be emptied 
 	 UDR=data;
  }
 
@@ -51,24 +51,16 @@ void led(int i)
 {
 	switch (i)
 	{
-		case 0 : PORTA|=(1<<PINA0);
-		
-		 break;
-		case 1 : PORTA|=(1<<PINA1);
-		
-		 break;
-		case 2 : PORTA|=(1<<PINA2);
-		
-		 break;
+		case 0 : PORTA|=(1<<PINA0);break;
+		case 1 : PORTA|=(1<<PINA1);break;
+		case 2 : PORTA|=(1<<PINA2);break;
 	}
 }
 
 void SPI_transfer(uint8_t data)
 {
-	//PORTB|=(1<<PINB4);							//set SS pin high every time 
-	//UART_send(data);							//check data sent to AD
 	SPDR=data;
-	while(!(SPSR&(1<<SPIF))) {;/*wait for data transfer and recieving*/} //Error is possible here
+	while(!(SPSR&(1<<SPIF))); 											//wait for data transfer and receiving
 }
 
 void UART_write16(unsigned short data)
@@ -92,7 +84,7 @@ void SPI_write16 (unsigned short data)    			//send a 16bit word and use f sync
 
 void Set_AD9833(float frequency)
 {
-	long FreqReg = (frequency*pow(2,28))/(float)Fmclk;  //Calculate frequency to be sent to AD9833
+	long FreqReg = (frequency*pow(2,28))/(float)Fmclk;	  //Calculate frequency to be sent to AD9833
 	int MSB = (int)((FreqReg &  0xFFFC000) >> 14);		  //Extract first 14 bits of FreqReg and place them at last 14 bits of MSB
 	int LSB = (int)((FreqReg & 0x3FFF));				  //Extract last 14 bits of FreqReg and place them at last 14 bits of MSB	
 	MSB|=0x4000;										  //Set D14,D15 = (1,0) for using FREQ0 registers, MSB has all 16 bits set
@@ -108,28 +100,34 @@ int main(void)
 {
 	UART_init();
 	SPI_init();
-	DDRA=(1<<PINA0)|(1<<PINA1)|(1<<PINA2);
+	
+	DDRA=(1<<PINA0)|(1<<PINA1)|(1<<PINA2);			//output pins for LEDs
 	TCCR1A=0;
 	
-{
-	// 	//test timer
-	// 	float cont;
-	// 	TCCR0|=(1<<CS10)|(1<<CS11);
-	// 	TCNT0=0;
-	// 	Set_AD9833(1900);
-	// 	cont=TCNT0;
-	// 160.590278
+	//test timer
+	{
+	//////////////////////////////////////////////////////////////////////////
+	// 	float cont;															//
+	// 	TCCR0|=(1<<CS10)|(1<<CS11);											//
+	// 	TCNT0=0;															//															
+	// 	Set_AD9833(1900);													//
+	// 	cont=TCNT0;															//
+	//  160.590278															//
+	//////////////////////////////////////////////////////////////////////////
 }
 
-	//color yellow
+	//color conversion from RGB to Y/RY/BY 
 	int R=255,G=0,B=0;
 	float Y = 16.0 + (.003906 * ((65.738 * R) + (129.057 * G) + (25.064 * B)));
 	float RY = 128.0 + (.003906 * ((112.439 * R) + (-94.154 * G) + (-18.285 * B)));
 	float BY = 128.0 + (.003906 * ((-37.945 * R) + (-74.494 * G) + (112.439 * B)));
-	float freqY  =  1500 + (Y * 3.1372549); //1757.2549(red)	1954.90196(green)	1628.62745(blue)	
-	float freqRY =  1500 + (RY * 3.1372549); //2252.94118(red)  1606.66667(green)	1845.09804(blue)
-	float freqBY =  1500 + (BY * 3.1372549); //1782.35294(red)	1669.41177(green)	2252.94118(blue)
-	SPI_write16(0x100);							//Reset AD9833 
+	//frequency calculation and documented values
+	float freqY  =  1500 + (Y * 3.1372549);			//1757.2549(red)	1954.90196(green)	1628.62745(blue)	
+	float freqRY =  1500 + (RY * 3.1372549);		//2252.94118(red)  1606.66667(green)	1845.09804(blue)
+	float freqBY =  1500 + (BY * 3.1372549);		//1782.35294(red)	1669.41177(green)	2252.94118(blue)|
+	
+	SPI_write16(0x100);								//Reset AD9833 
+	
 	//VIS Code
 	{//leader tone
 	Set_AD9833(1900);
@@ -173,9 +171,9 @@ int main(void)
 	_delay_ms(30);
 	}
 	
+	//image data
 	for (int i=1;i<=128;i++)
 	{
-	PORTA=0;
 	//Sync Pulse
 	Set_AD9833(1200);
 	_delay_ms(19);
@@ -185,17 +183,18 @@ int main(void)
 	_delay_ms(1);
 	_delay_us(920);
 	//Color transmission
+	
+	//single color using interrupts
+	
+	{
 	//Y Scan odd line
 	cont=0;	
 	global_frequency=freqY;	
 	sei();
 	TCCR1B|=(1<<CS10);	
 	TIMSK|=(1<<TOIE1);
-	TCNT1=66534;
-	while (cont<1)
-	{
-		;
-	} 
+	TCNT1=65534;
+	while (cont<1);
 	cli();
 	TIMSK&=~(1<<OCIE1A);
 	TCCR1B=0x00;
@@ -206,10 +205,7 @@ int main(void)
 	TCCR1B|=(1<<CS10);
 	TIMSK|=(1<<TOIE1);
 	TCNT1=TICKS;
-	while (cont<1)
-	{
-		;
-	}
+	while (cont<1);
 	cli();
 	TIMSK&=~(1<<OCIE1A);
 	TCCR1B=0x00;
@@ -220,10 +216,7 @@ int main(void)
 	TCCR1B|=(1<<CS10);
 	TIMSK|=(1<<TOIE1);
 	TCNT1=TICKS;
-	while (cont<1)
-	{
-		;
-	}
+	while (cont<1);
 	cli();
 	TIMSK&=~(1<<OCIE1A);
 	TCCR1B=0x00;
@@ -234,13 +227,12 @@ int main(void)
 	TCCR1B|=(1<<CS10);
 	TIMSK|=(1<<TOIE1);
 	TCNT1=TICKS;
-	while (cont<2)
-	{
-		;
-	}
+	while (cont<2);
 	cli();
 	TIMSK&=~(1<<OCIE1A);
 	TCCR1B=0x00;
+	}
+	
 	
 	{
 // 		//Y Scan odd line
@@ -288,7 +280,6 @@ int main(void)
 // 		_delay_us(170079.41);
 }
 }	
-	
 	PORTA=0; PORTA|=(1<<PINA0);
 	while(1)
 	{
